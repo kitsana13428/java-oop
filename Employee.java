dd38ffcb8b4eaec93f057df757631d23c45fc584
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-07-06 23:55:55 +0700

    +

--- Employee.java
+++ Employee.java
@@ -3,4 +3,12 @@ public class Employee {
     private String id;
     private String name;
     private String salary;
+
+    public void setID(String id){ //สร้าง Method เพื่อเรียกใช้ ไปกำหนดค่า
+        this.id=id; 
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
 }
--- Main.java
+++ Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Employee e1 = new Employee(); //Create Object
         
+        e1.setID("1");
+        e1.setName("Kitsana");
+
+    
     }
 }
