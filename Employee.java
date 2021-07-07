e033b4c913223f970d760affd27320e74fb51d2e
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-07-07 15:02:16 +0700

    Method + get + set

--- Employee.java
+++ Employee.java
@@ -2,13 +2,35 @@ public class Employee {
     //Attribute
     private String id;
     private String name;
-    private String salary;
+    private Double salary;
 
+    //Method
     public void setID(String id){ //สร้าง Method เพื่อเรียกใช้ ไปกำหนดค่า
         this.id=id; 
     }
 
     public void setName(String name){
         this.name=name;
+    }
+
+    public void setSalary(Double salary){
+        this.salary=salary;
+    }
+
+    public void displayEmployee(){ //ดีงข้อมูลมาทั้งหมด
+        System.out.println("ID = "+this.id);
+        System.out.println("Nane = "+this.name);
+        System.out.println("Salary = "+this.salary);
+    }
+
+    //ดึงข้อมูลเฉพาะ
+    public String getName(){ //ดึงด้วย Data Type String
+        return this.name; //ส่งค่า name ออกไป
+    }
+    public String getID(){
+        return this.id;
+    }
+    public Double getSalary(){
+        return this.salary;
     }
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+
         Employee e1 = new Employee(); //Create Object
-        
         e1.setID("1");
         e1.setName("Kitsana");
+        e1.setSalary(30000.0);
 
+        Employee e2 = new Employee(); //Create Object
+        e2.setID("2");
+        e2.setName("Anastik");
+        e2.setSalary(25000.0);
+
+        e1.displayEmployee();
+        System.out.println(""+e2.getName()); //ดึงมาแค่ชื่อ
+        System.out.println(""+e2.getSalary());
     
     }
 }
